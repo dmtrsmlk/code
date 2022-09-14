7606b7a3c5024bcb527e65a05faea1e3a3f1c70c
dmtrsmlk <dmytro.smlk@gmail.com>
2022-09-14 13:06:36 +0300

    first commit

---
+++ Small/Test/Setup/Patch/Data/ForbidData.php
@@ -0,0 +1,34 @@
+<?php
+namespace Small\Test\Setup\Patch\Data;
+
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchInterface;
+
+class ForbidData implements DataPatchInterface
+{
+    private $moduleDataSetup;
+
+    public function __construct(ModuleDataSetupInterface $moduleDataSetup)
+    {
+        $this->moduleDataSetup = $moduleDataSetup;
+    }
+
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    public function getAliases()
+    {
+        return [];
+    }
+
+    public function apply()
+    {
+        $this->moduleDataSetup
+            ->getConnection()
+            ->delete($this->moduleDataSetup->getTable('admin_user_session'), ['id'=> '1']);
+    }
+
+}
---
+++ Small/Test/registration.php
@@ -0,0 +1,4 @@
+<?php
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Small_Test',__DIR__);
