d018e2f2792e94728a192e6fb1bf87ae5ddcaf38
dmtrsmlk <dmytro.smlk@gmail.com>
2022-09-21 18:14:05 +0300

    added renewed controller

---
+++ Small/Test/Controller/Index/Test.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Small\Test\Controller\Index;
+
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\View\Result\PageFactory;
+
+class Test implements HttpGetActionInterface
+{
+    private $pageFactory;
+    public function __construct(
+        PageFactory $pageFactory
+    )
+    {
+        $this->pageFactory = $pageFactory;
+    }
+
+    public function execute()
+    {
+        return $this->pageFactory->create();
+    }
+}
