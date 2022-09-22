cb17bacc674f139ba7e2b9ea16334a11bd6716f2
dmtrsmlk <dmytro.smlk@gmail.com>
2022-09-22 17:55:59 +0300

    added Checkout Step

---
+++ Small/Test/view/frontend/web/js/view/checkout-login-step.js
@@ -0,0 +1,70 @@
+define(
+    [
+        'ko',
+        'uiComponent',
+        'underscore',
+        'Magento_Checkout/js/model/step-navigator',
+        'Magento_Customer/js/model/customer'
+    ],
+    function (
+        ko,
+        Component,
+        _,
+        stepNavigator,
+        customer
+    ) {
+        'use strict';
+        /**
+         * check-login - is the name of the component`s .html template
+         */
+        return Component.extend({
+            defaults: {
+                template: 'Small_Test/check-login'
+            },
+
+            //add here your logic to display step,
+            isVisible: ko.observable(true),
+            isLoggedIn: customer.isLoggedIn(),
+            //step code will be used as step content id in the component template
+            stepCode: 'isLoggedCheck',
+            //step title value
+            stepTitle: 'Logging Status',
+
+            initialize: function () {
+                this._super();
+                // register your step
+                stepNavigator.registerStep(
+                    this.stepCode,
+                    //step alias,
+                    null,
+                    this.stepTitle,
+                    //observable property with logic when display step or hide step
+                    this.isVisible,
+                    _.bind(this.navigate, this),
+                    /**
+                     * sort order value
+                     * 'sort order value' < 10: step displays before shipping step
+                     * 10 < 'SOV' < 20: step displays between shipping and payment step
+                     * 'SOV' > 20: step displays after payment step
+                     */
+                    15
+                );
+
+                return this;
+            },
+
+            /**
+             * navigate() method is responsible for navigation between checkout step
+             * during checkout. You can add custom logic, for example some conditions
+             * for switching to your custom step
+             */
+           navigate: function (){
+
+            },
+
+            navigateToNextStep: function (){
+               stepNavigator.next();
+            }
+        });
+    }
+);
